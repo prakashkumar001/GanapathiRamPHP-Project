d44b529357930bc75c4c403d89b8781c53acb5e8
Prakash A <prakash@dexeldesigns.com>
2017-12-13 14:26:01 +0530

    logout

--- app/src/main/java/com/ganapathyram/theatre/activities/Home.java
+++ app/src/main/java/com/ganapathyram/theatre/activities/Home.java
@@ -8,6 +8,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
